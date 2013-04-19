921fbe927cd3d290ceb6a13d3af348d7db894b24
Bjorn Newkirk Grambow <bjoern.grambow@gmail.com>
2013-04-19 12:04:58 +0100

    fix broken commit

---
+++ config/site.php
@@ -0,0 +1,8 @@
+<?php 
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'a_brandrehab');
+define('DB_PASSWORD', 'p6ngu1n');
+define('DB_DATABASE', 'brandrehab');
+define('BASE_URL', 'http://brandrehab.dev1.ngoar.com');
+define('DIR_REL', '');
+define('PASSWORD_SALT', '3UkRfBtzrwxbtG8sJxUXt7oX8iSA0x6GKDWDnofsI5eJczHXKbbNexI2kM2Ts2O7');?><?php define('DIRNAME_APP_UPDATED', 'concrete5.6.0.2');?>
